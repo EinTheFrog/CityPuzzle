a1e481aa64892e4dab73d5fb18c17af0ecb1e852
EinTheFrog <MaximAlpatski@gmail.com>
2021-11-27 20:25:38 +0300

    Fixed earth rotation

--- Assets/Scripts/EarthBehaviour.cs
+++ Assets/Scripts/EarthBehaviour.cs
@@ -5,20 +5,45 @@ using UnityEngine;
 
 public class EarthBehaviour : MonoBehaviour
 {
-    private Animator _anim;
+    [SerializeField] private float rotationSpeed = 1;
+    
+    private float _targetY;
+    private int _rotationDir;
 
     private void Start()
     {
-        _anim = GetComponent<Animator>();
+        _targetY = transform.eulerAngles.y;
+        _rotationDir = 0;
+    }
+
+    private void Update()
+    {
+        var eulerAngles = transform.eulerAngles;
+        var step = rotationSpeed * Time.deltaTime;
+        var distance = Mathf.Abs(_targetY - eulerAngles.y);
+        if (distance < step)
+        {
+            transform.eulerAngles = new Vector3(eulerAngles.x, _targetY, eulerAngles.z);
+            return;
+        }
+
+        var newY = (eulerAngles.y + _rotationDir * step) % 360;
+        transform.eulerAngles = new Vector3(eulerAngles.x, newY, eulerAngles.z);
     }
 
     public void RotateRight()
     {
-        _anim.Play("EarthRightRotation");
+        if (Math.Abs(transform.eulerAngles.y - _targetY) > 0.01) return;
+        var eulerAngles = transform.eulerAngles;
+        _targetY = (eulerAngles.y + 90) % 360;
+        _rotationDir = 1;
     }
 
     public void RotateLeft()
     {
-        _anim.Play("EarthLeftRotation");
+        if (Math.Abs(transform.eulerAngles.y - _targetY) > 0.01) return;
+        var eulerAngles = transform.eulerAngles;
+        _targetY = (eulerAngles.y + 270) % 360;
+        _rotationDir = -1;
     }
 }
